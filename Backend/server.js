9b2806367074055de663a086ab9153a0f9a13660
minh-tri <minh-tri@system-exe.com.vn>
2023-04-03 12:20:38 +0700

    completed lession 1

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,19 @@ const path = require("path");
 const PORT = process.env.PORT || "8080";
 //require("dotenv").config();
 
-app.use("/", (req, res) => {
-  res.send("Hello world");
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("html")) {
+    res.josn({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.listen(PORT, () => {
